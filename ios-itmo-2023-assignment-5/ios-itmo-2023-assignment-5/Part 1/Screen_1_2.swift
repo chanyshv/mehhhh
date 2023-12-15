f01f084e27c24c26349afeb315fd426f4873f429
chanyshev <chanyshv@gmail.com>
2023-12-15 23:52:57 +0300

    initial

--- ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_2.swift
+++ ios-itmo-2023-assignment-5/ios-itmo-2023-assignment-5/Part 1/Screen_1_2.swift
@@ -1,12 +1,61 @@
 import SwiftUI
 
 struct Screen_1_2: View {
+    @State private var password: String = ""
 
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+        VStack {
+            HStack {
+                Image("v")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 30, height: 30)
+                Text("ID")
+                    .font(.title)
+                    .fontWeight(.medium)
+            }
+            .padding(.top, 40)
 
+
+
+            Text("Введите пароль")
+                .font(.title)
+                .padding()
+
+            SecureField("Введите пароль", text: $password)
+                .padding()
+                .background(Color.gray.opacity(0.2))
+                .cornerRadius(12)
+                .padding(.horizontal)
+
+            HStack {
+                Button(action: {
+                }) {
+                    Text("Забыли или не установили пароль?")
+                        .foregroundColor(.blue)
+                }
+                Spacer()
+            }
+            .padding([.leading, .top])
+
+            Spacer()
+            Button(action: {
+            }) {
+                Text("Продолжить")
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding()
+
+        }
+        .padding(.horizontal)
+    }
 }
+
+
 
 #Preview {
     Screen_1_2()
